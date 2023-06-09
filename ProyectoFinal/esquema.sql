d32368e1eda0880da1f717bc1bb1dfce09a244f0
user23007 <115121523+user23007@users.noreply.github.com>
2023-06-09 15:07:06 -0500

    Update esquema.sql

---
+++ ProyectoFinal/esquema.sql
@@ -0,0 +1,22 @@
+mysql -u root -p
+
+create database universidad;
+
+use universidad;
+
+create table usuarios(
+    id_usuario integer not null auto_increment,
+    nombre varchar(100),
+    email varchar(100),
+    genero varchar(30),
+    contraseña varchar(200),
+    primary key(id)
+);
+
+create user "dbuser"@"%" identified with mysql_native_password BY "Eafit2023.";
+
+grant all privileges on universidad.* to "dbuser"@"%";
+flush privileges;
+
+insert into usuarios (nombre,email,genero,contraseña) values("sebas","sebastiancano21@hotmail.es","fluido","holi1234");
+insert into usuarios (nombre,email,genero,contraseña) values("ana","anita123@gmail.com","hetere","ano");
